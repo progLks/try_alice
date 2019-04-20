09d4e48a8db94ddf9a3c1549f479ba3035c3a054
alex_alice <lexofour70@gmail.com>
2019-04-20 15:19:11 +0300

    update project

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import logging
+# import logging
 from alice_sdk import *
 from main_functions import *
 
@@ -15,12 +15,12 @@ def hello_world():
 def main():
     alice_request = AliceRequests(request.json)
     alice_response = AliceResponse(alice_request)
-    logging.info("Request {}".format(alice_request))
+    # logging.info("Request {}".format(alice_request))
     session_storage = {}
     alice_response, session_storage[alice_request.user_id] = handle_dialog(
         alice_request, alice_response, session_storage.get(alice_request.user_id))
 
-    logging.info("Response {}".format(alice_response))
+    #logging.info("Response {}".format(alice_response))
 
     return alice_response.dumps()
 
--- main_functions.py
+++ main_functions.py
@@ -2,5 +2,7 @@ from little_functions import *
 
 
 def handle_dialog(request, response, user_storage):
+    if not user_storage:
+        user_storage = {}
     output_message = "Привет, епта"
     return message_return(response, user_storage, output_message)
